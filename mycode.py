a59ec60f87d4ec30d4f1cf0bb3689472f5b4947e
ofersabo <ofersabo@gmail.com>
2019-05-02 09:12:51 +0300

    normalize over feature or time

--- mycode.py
+++ mycode.py
@@ -2,11 +2,28 @@ import librosa
 import os
 import numpy as np
 
-
 train_dir = "data/train_data"
 test_dir = "data/test_files"
+to_normelaize = True
+transalte = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
+axis_to_normelaize = 1
 
-transalte = {'one':1, 'two':2, 'three':3 , 'four':4,'five':5}
+two_axis_normaliztion = 0\
+
+
+def normalize(v, by_axis = axis_to_normelaize):
+    global ll
+    sum_col = np.mean(v, axis=by_axis).reshape(v.shape[1 - by_axis], -1)
+    std_col = np.std(v, axis=by_axis).reshape(v.shape[1 - by_axis], -1)
+    if by_axis == 1:
+        res = (v - sum_col) / std_col
+    else:
+        res = (v - sum_col.T) / std_col.T
+
+    if two_axis_normaliztion == 1 and by_axis == 1:
+        return normalize(res, 1 - by_axis)
+    return res
+
 
 def eudis(mfcc, sound):
     dist = (mfcc - sound) ** 2
@@ -15,67 +32,30 @@ def eudis(mfcc, sound):
     return dist
 
 
-
 def DTW(mfcc, sound):
     def find_shortest_path(matrix):
-        i = len(matrix) - 1
-        j = len(matrix) - 1
-        total = matrix.item(i,j)
-        while i > 0 and j >0:
-            which_min = np.argmin( [matrix.item(i-1,j-1), matrix.item(i-1,j),matrix.item(i,j-1)])
-            if which_min == 0:
-                i-=1
-                j-=1
-                total += matrix.item(i-1,j-1)
-            elif which_min == 1:
-                i-=1
-                total += matrix.item(i-1,j)
-            else:
-                j-=1
-                total += matrix.item(i,j-1)
-        while i > 0:
-            i -= 1
-            total += matrix.item(i - 1, j)
-        while j > 0:
-            j -= 1
-            total += matrix.item(i, j - 1)
-        return total
-
+        path = np.zeros_like(matrix)
+        top_i = len(matrix)
+        top_j = len(matrix)
+        path[0] = np.cumsum(matrix[0])
+        path[:, 0] = np.cumsum(matrix[:, 0]).T
+        for j in range(1, top_j):
+            for i in range(1, top_i):
+                path[i, j] = matrix.item(i, j)
+                path[i, j] += np.min([path.item(i - 1, j - 1), path.item(i - 1, j), path.item(i, j - 1)])
+                # which_min = np.argmin( [path.item(i-1,j-1), path.item(i-1,j),path.item(i,j-1)])
+                # loc = [(i-1,j-1),(i-1,j),(i,j-1)][which_min]
+                # path[i, j] += path.item(loc)
+        return path.item(top_i - 1, top_j - 1)
 
     assert mfcc.shape == sound.shape
     mat = []
     for v in mfcc:
-        dist = eudis(v,sound)
+        dist = eudis(sound, v)
         mat.append(dist)
     matrix = np.asmatrix(mat)
-    matrix[0,0] = 0
     return find_shortest_path(matrix)
 
-# def get_gold_anntotion(file):
-#     import speech_recognition as sr
-#     harvard = sr.AudioFile(file)
-#     r = sr.Recognizer()
-#     with harvard as source:
-#         audio = r.record(source)
-#     print(file)
-#     try:
-#         digit = r.recognize_google(audio)
-#     except:
-#         try:
-#             digit = r.recognize_ibm(audio)
-#         except:
-#             print("isnt known")
-#             digit = '0'
-#     print(digit)
-#     try:
-#         return transalte[digit]
-#     except:
-#         if digit in str(transalte.values()):
-#             return digit
-#         else:
-#             print("error detection file was thought to be ", digit)
-#             return "0"
-#             exit()
 
 def prepare_train_data(train_directory):
     train = {}
@@ -87,47 +67,55 @@ def prepare_train_data(train_directory):
                 filepath = subdir + os.sep + file
                 y, sr = librosa.load(filepath, sr=None)
                 mfcc = librosa.feature.mfcc(y=y, sr=sr)
+                if to_normelaize:
+                    mfcc = normalize(mfcc)
                 digit_list = train.get(digit_name, [])
                 digit_list.append(mfcc)
                 train[digit_name] = digit_list
+    for digit, item in train.items():
+        assert len(item) == 5
     return train
 
+
 from collections import Counter
+
 euclid = Counter()
 dtw = Counter()
 text_to_file = []
 gold_text = []
 train_data = prepare_train_data(train_dir)
 for test_file in os.listdir(test_dir):
+    if not test_file.endswith(".wav"): continue
     euclid_scores = {}
     dtw_scores = {}
     row_text = test_file
     test_filename = os.path.join(test_dir, test_file)
-    if not test_filename.endswith(".wav"): continue
     y, sr = librosa.load(test_filename, sr=None)
     mfcc = librosa.feature.mfcc(y=y, sr=sr)
-    for digit,items in train_data.items():
+    for digit, items in train_data.items():
         eculid_distance = []
-        DTW_distance    = []
+        DTW_distance = []
         for wav_train_sound in items:
-            eculid_distance.append(eudis(mfcc.T, wav_train_sound.T).sum())
+            if to_normelaize:
+                mfcc = normalize(mfcc)
+
+            eculid_distance.append(eudis(mfcc.T, wav_train_sound.T).mean())
             DTW_distance.append(DTW(mfcc.T, wav_train_sound.T))
         eculid_distance = min(eculid_distance)
         DTW_distance = min(DTW_distance)
 
         euclid_scores[digit] = eculid_distance
-        dtw_scores[digit]    = DTW_distance
+        dtw_scores[digit] = DTW_distance
 
     sorted_euclid = sorted(euclid_scores.items(), key=lambda euclid_scores: euclid_scores[1])
     sorted_dtw = sorted(dtw_scores.items(), key=lambda dtw_scores: dtw_scores[1])
-    # gold_annotation = get_gold_anntotion(test_filename)
 
     euclid.update([transalte[sorted_euclid[0][0]]])
     dtw.update([transalte[sorted_dtw[0][0]]])
-    text_to_file.append(row_text + ' - ' + str(transalte[sorted_euclid[0][0]])  + ' - ' + str(transalte[sorted_dtw[0][0]]) + "\n")
-    # gold_text.append(row_text + ' - ' + str(gold_annotation) + "\n")
+    text_to_file.append(
+        row_text + ' - ' + str(transalte[sorted_euclid[0][0]]) + ' - ' + str(transalte[sorted_dtw[0][0]]) + "\n")
 
 open("output.txt", 'w').writelines(text_to_file)
-# open("gold_output.txt", 'w').writelines(gold_text)
-# print(euclid)
-# print(dtw)
+
+print(euclid)
+print(dtw)
